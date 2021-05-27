c3199e2ff9cb05981f7afa9722bbb4596b794af8
AS <arunsalgia@gmail.com>
2021-05-27 11:59:36 +0530

    APL/build in app.js

--- app.js
+++ app.js
@@ -64,9 +64,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'APL/build/')));
+//app.use(express.static(path.join(__dirname, 'APL/build/')));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('APL/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'APL/build', 'index.html'));
+});
 
 app.use((req, res, next) => {
   if (req.url.includes("admin")||req.url.includes("signIn")||req.url.includes("Logout")) {
